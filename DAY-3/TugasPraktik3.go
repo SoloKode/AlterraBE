d010d9446069555ab882ce0ac495c3241b4dff5c
Doffel <syaidahmad7z@gmail.com>
2023-10-20 22:15:42 +0700

    Penyelesaian TugasPraktik Day 3

--- DAY-3/TugasPraktik3.go
+++ DAY-3/TugasPraktik3.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func TugasMergeArray() {
 	fmt.Println(ArrayMerge([]string{"king", "devil jin", "akuma"}, []string{"eddie", "steve", "geese"}))
@@ -16,7 +18,6 @@ func TugasMergeArray() {
 	fmt.Println(ArrayMerge([]string{}, []string{}))
 	// Output yang diharapkan: []
 }
-
 func ArrayMerge(arrayA, arrayB []string) []string {
 	mergeArray := make([]string, 0)
 	mergeArray = append(mergeArray, arrayA...)
@@ -36,15 +37,19 @@ func ArrayMerge(arrayA, arrayB []string) []string {
 	return mergeArray
 }
 func main() {
+
 	var pilihan uint8
 	fmt.Println("1. Tugas Merge Array")
 	fmt.Println("2. Tugas Slice Map")
+	fmt.Println("2. Tugas Bilangan Muncul Sekali")
 	fmt.Print("Masukkan Pilihan : ")
 	fmt.Scanln(&pilihan)
 	if pilihan <= 1 {
 		TugasMergeArray()
-	} else {
+	} else if pilihan == 2 {
 		TugasMappingSlice()
+	} else {
+		TugasMunculSekali()
 	}
 }
 func Mapping(slice []string) map[string]int {
@@ -57,8 +62,32 @@ func Mapping(slice []string) map[string]int {
 	}
 	return dataSlice
 }
-
 func TugasMappingSlice() {
 	fmt.Println(Mapping([]string{"asd", "qwe", "asd", "adi", "qwe", "qwe"})) // map[asd:0 qwe:1 adi:3]
 	fmt.Println(Mapping([]string{}))
+}
+
+func munculSekali(angka string) []int {
+	muncul := make(map[int]int)
+	for _, digit := range angka {
+		digitInt := int(digit - '0')
+		muncul[digitInt]++
+	}
+	sekali := []int{}
+	for _, digit := range angka {
+		digitInt := int(digit - '0')
+		if muncul[digitInt] == 1 {
+			sekali = append(sekali, digitInt)
+		}
+	}
+
+	return sekali
+}
+
+func TugasMunculSekali() {
+	fmt.Println(munculSekali("1234123"))    // [4]
+	fmt.Println(munculSekali("76523752"))   // [6 3]
+	fmt.Println(munculSekali("12345"))      // [1 2 3 4 5]
+	fmt.Println(munculSekali("1122334455")) // []
+	fmt.Println(munculSekali("0872504"))    // [8 7 2 5 4]
 }
